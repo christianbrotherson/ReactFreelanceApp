c5a98548a6c73ee1bf7cbb28d5cfaa9e091b8175
Christian Brotherson <christian.brotherson@gmail.com>
2018-04-04 14:10:38 -0600

    modified the handleChange function to appecpt props and setState within the function

--- src/components/freelance_form.js
+++ src/components/freelance_form.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  Col,
+  Row
+} from 'reactstrap';
 
 class FreeLanceForm extends Component {
 
@@ -6,33 +10,37 @@ class FreeLanceForm extends Component {
     super(props)
 
     this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      customer: '',
+      portfolioUrl: '',
+      tools: '',
+      hourlyRate: '',
+      weeksToComplete: '',
+    }
   }
 
-  handleChange() {
-    console.log('trying to handle change');
+  handleChange (props) {
+    return function(event) {
+      this.setState({ [props.inputTitle]: event.target.value });
+      console.log(`value for state ${props.inputTitle} is: ${this.state[props.inputTitle]}`);
+    }.bind(this)
   }
 
   render() {
-    this.inputData = [
-      { placeholder: 'Customer', prop: 'customer' },
-      { placeholder: 'Portfolio URL', prop: 'portfolioUrl' },
-      { placeholder: 'Tools', prop: 'tools' },
-      { placeholder: 'Estimated Hours', prop: 'estimatedHours' },
-      { placeholder: 'Hourly Rate', prop: 'hourlyRate' },
-      { placeholder: 'Weeks to Complete', prop: 'weeksToComplete' },
-    ]
-
     return(
       <div>
-        <div>Proposal Inputs</div>
-        <div>
-          <input placeholder={this.inputData[0].placeholder} onChange={this.handleChange} />
-          <input placeholder={this.inputData[1].placeholder} onChange={this.handleChange} />
-          <input placeholder={this.inputData[2].placeholder} onChange={this.handleChange} />
-          <input placeholder={this.inputData[3].placeholder} onChange={this.handleChange} />
-          <input placeholder={this.inputData[4].placeholder} onChange={this.handleChange} />
-          <input placeholder={this.inputData[5].placeholder} onChange={this.handleChange} />
-        </div>
+        <div><h3>Proposal Inputs</h3></div>
+        <Row>
+          <Col md='12'>
+            <input placeholder='customer' type='text' onChange={this.handleChange({inputTitle: 'customer'})} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md='12'>
+            <div>Customer</div>
+          </Col>
+        </Row>
       </div>
     );
   }
